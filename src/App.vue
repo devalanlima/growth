286a352a56c543ff75afc1e73afe234cfaa85498
Alan Lima <dev.alanlima@gmail.com>
2023-10-05 13:29:59 -0300

    feat: Add main page to App component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    App
-  </div>
+  <PageMain/>
 </template>
+
+<script setup lang="ts">
+import PageMain from './components/pages/PageMain.vue';
+</script>
+
+<style scoped>
+
+</style>
