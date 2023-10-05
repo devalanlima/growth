e2015759e5a52636a4ec1a19153c2bf263b3c4e9
Alan Lima <dev.alanlima@gmail.com>
2023-10-05 13:22:19 -0300

    feat: Create menu button

---
+++ src/components/atoms/AtomDropdownButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button 
+    class="dropdown-button"
+    @click="action"
+    >
+        <div class="line"></div>
+        <div class="line"></div>
+    </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['action'])
+
+const action = ()=> emit('action')
+</script>
+
+<style scoped>
+.dropdown-button {
+    width: 40px;
+    height: 40px;
+    flex-shrink: 0;
+    border-radius: 12px;
+    background: #F5F5F5;
+    position: relative;
+    display: flex;
+    gap: 4px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.line{
+    display: block;
+    width: 20px;
+    height: 2px;
+    background-color: black;
+    position: relative;
+}
+</style>
