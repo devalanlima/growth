bce00bccb329f6d4211fd337cd00721d0375184c
Alan Lima <dev.alanlima@gmail.com>
2023-10-05 13:19:45 -0300

    feat: Create paragraph component

---
+++ src/components/atoms/AtomText.vue
@@ -0,0 +1,21 @@
+<template>
+    <p class="paragraph">{{ props.paragraph }}</p>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    paragraph: String
+}
+const props = defineProps<Props>()
+</script>
+
+<style scoped>
+.paragraph {
+    color: var(--color-five);
+    font-family: Poppins;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%; /* 25.6px */
+}
+</style>
