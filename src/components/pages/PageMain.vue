11e47a62e8fa8fe43b7d8a4a05ab40c3dbe73b51
Alan Lima <dev.alanlima@gmail.com>
2023-10-05 13:25:37 -0300

    feat: Create main page component

---
+++ src/components/pages/PageMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="base-header">
+        <BaseNavbar/>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+</template>
+
+<script setup lang="ts">
+import BaseNavbar from '../organisms/BaseNavbar.vue';
+</script>
+
+<style scoped>
+.base-header{
+    padding: 24px 16px;
+}
+</style>
