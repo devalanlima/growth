8ba7052a2273f3e079ab7d066e1cfa659d4906b7
Alan Lima <dev.alanlima@gmail.com>
2023-10-06 10:30:42 -0300

    feat: Create introduction section component

---
+++ src/components/organisms/OrgIntroduction.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="wrapper">
+        <MolContenth1
+        base-title="A powerful solution to grow your startup. Fast!"
+        base-text="Organise, collaborate, and track progress seamlessly with our one-stop-shop startup growth tool."
+        />
+
+        <div class="btn-wrapper">
+            <AtomBaseButton
+            bnt-name="Get started"
+            btn-width="full"
+            bg-color="color-one"
+            />
+            <AtomBaseButton
+            :left-icon="IconWebCam"
+            bnt-name="Book a demo"
+            btn-width="full"
+            bg-color="color-two"
+            />
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import AtomBaseButton from '../atoms/AtomBaseButton.vue';
+import IconWebCam from '../atoms/Icons/IconWebCam.vue';
+import MolContenth1 from '../molecules/MolContentH1.vue';
+</script>
+
+<style scoped>
+.wrapper{
+    display: flex;
+    flex-direction: column;
+    gap: 32px; 
+}
+
+.btn-wrapper{
+    display: flex;
+    flex-direction: column;
+    gap: 19px;
+}
+</style>
