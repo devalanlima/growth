42199c576171f9ba1ed544df208593231277381a
Alan Lima <dev.alanlima@gmail.com>
2023-10-06 10:37:00 -0300

    feat: Add introduction component to main page

--- src/components/pages/PageMain.vue
+++ src/components/pages/PageMain.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="base-header">
-        <BaseNavbar/>
+        <OrgBaseNavbar/>
     </header>
-    <main>
-
+    <main class="base-main">
+        <OrgIntroduction/>
     </main>
     <footer>
         
@@ -11,11 +11,19 @@
 </template>
 
 <script setup lang="ts">
-import BaseNavbar from '../organisms/BaseNavbar.vue';
+import OrgBaseNavbar from '../organisms/OrgBaseNavbar.vue';
+import OrgIntroduction from '../organisms/OrgIntroduction.vue';
+
+
 </script>
 
 <style scoped>
 .base-header{
-    padding: 24px 16px;
+    padding: 24px 16px 0px 16px;
+}
+
+.base-main{
+    margin-top: 84px;
+    padding: 0 16px;
 }
 </style>
