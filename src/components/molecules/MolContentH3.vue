ca090b65b7da13fecc3a476c7a7f7f781aebc2ff
Alan Lima <dev.alanlima@gmail.com>
2023-10-09 11:53:44 -0300

    style: Add gap

--- src/components/molecules/MolContentH3.vue
+++ src/components/molecules/MolContentH3.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="wrapper">
         <h3 class="base-title">{{ props.baseTitle }}</h3>
         <p class="base-paragraph">{{ props.baseText }}</p>
     </div>
@@ -15,10 +15,14 @@ const props = defineProps<Props>();
 </script>
 
 <style scoped>
+.wrapper{
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+}
 .base-title {
     margin: 0;
     font-size: 24px;
-    line-height: 120%;
-    /* 28.8px */
+    line-height: 120%; /* 28.8px */
 }
 </style>
