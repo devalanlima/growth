e8eec5de0d2767135469184387a50dd94e9c0449
Alan Lima <dev.alanlima@gmail.com>
2023-10-09 14:02:04 -0300

    feat: Add How it Works section to main page

--- src/components/pages/PageMain.vue
+++ src/components/pages/PageMain.vue
@@ -3,8 +3,11 @@
         <OrgBaseNavbar/>
     </header>
     <main class="base-main">
-        <OrgIntroduction/>
-        <OrgEssentials/>
+        <div class="wrapper-first">
+            <OrgIntroduction/>
+            <OrgEssentials/>
+        </div>
+        <OrgHowItWorks/>
     </main>
     <footer>
         
@@ -14,19 +17,26 @@
 <script setup lang="ts">
 import OrgBaseNavbar from '../organisms/OrgBaseNavbar.vue';
 import OrgEssentials from '../organisms/OrgEssentials.vue';
+import OrgHowItWorks from '../organisms/OrgHowItWorks.vue';
 import OrgIntroduction from '../organisms/OrgIntroduction.vue';
 
 
 </script>
 
 <style scoped>
+
+.wrapper-first{
+    padding: 0 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 63px;
+}
 .base-header{
     padding: 24px 16px 0px 16px;
 }
 
 .base-main{
     margin-top: 84px;
-    padding: 0 16px;
     display: flex;
     flex-direction: column;
     gap: 63px;
