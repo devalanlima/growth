4ce672ed13bd9e1723fa6a54d4b3a7f7a1a99617
Alan Lima <dev.alanlima@gmail.com>
2023-10-09 14:09:12 -0300

    feat: Create how it works component section

---
+++ src/components/organisms/OrgHowItWorks.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="base-section">
+        <MolHowWorksVideo />
+        <hr class="base-hr">
+        <MolHowWorksComments/>
+    </section>
+</template>
+
+<script setup lang="ts">
+import MolHowWorksComments from "../molecules/MolHowWorksComments.vue";
+import MolHowWorksVideo from "../molecules/MolHowWorksVideo.vue";
+
+</script>
+
+<style scoped>
+.base-section {
+    background-color: var(--color-two);
+    padding: 64px 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 64px;
+}
+
+.base-hr {
+    width: 343px;
+    border:1px solid var(--color-five);
+}
+</style>
+
