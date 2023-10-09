1a59ad67a84468db504d97b160b84f80559033ed
Alan Lima <dev.alanlima@gmail.com>
2023-10-09 11:56:48 -0300

    feat: Create learn more component

---
+++ src/components/atoms/AtomLearnMore.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="base-button">
+        Learn more <span>
+            <IconArrow />
+        </span>
+    </button>
+</template>
+
+<script setup lang="ts">
+import IconArrow from './Icons/IconArrow.vue';
+
+
+</script>
+
+<style scoped>
+.base-button {
+    color: var(--color-two);
+    font-family: Poppins;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -0.32px;
+
+    display: flex;
+    align-items: center;
+    gap: 8px;
+}
+</style>
