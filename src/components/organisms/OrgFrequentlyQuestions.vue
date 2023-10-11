0074063a2db511a7fb8227d928ddcb23531fdb1d
Alan Lima <dev.alanlima@gmail.com>
2023-10-11 12:06:07 -0300

    feat: Create frequently questions section

---
+++ src/components/organisms/OrgFrequentlyQuestions.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="questions-wrapper">
+        <h2 class="base-h2">Frequently asked questions</h2>
+        <MolAskedQuestions/>
+    </section>
+</template>
+
+<script setup lang="ts">
+import MolAskedQuestions from '../molecules/MolAskedQuestions.vue';
+
+
+</script>
+
+<style scoped>
+.questions-wrapper{
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+}
+.base-h2{
+    text-align: center;
+}
+</style>
