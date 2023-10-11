78c9a5e15ef197eba0f49846654693a96884adea
Alan Lima <dev.alanlima@gmail.com>
2023-10-11 12:02:13 -0300

    style: Add min size to icon

--- src/components/atoms/Icons/IconPlus.vue
+++ src/components/atoms/Icons/IconPlus.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="base-icon" width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g clip-path="url(#clip0_1_764)">
             <path d="M21 10.1499H0.699951" stroke="#131316" stroke-width="2" stroke-miterlimit="10" />
             <path d="M10.8484 20.3V0" stroke="#131316" stroke-width="2" stroke-miterlimit="10" />
@@ -11,3 +11,10 @@
         </defs>
     </svg>
 </template>
+
+<style scoped>
+.base-icon {
+    width: 15px;
+    min-width: 15px;
+}
+</style>
--- src/components/atoms/Icons/IconX.vue
+++ src/components/atoms/Icons/IconX.vue
@@ -1,6 +1,13 @@
 <template>
-    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="base-icon" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M16 1.00023L1.64575 15.3545" stroke="#131316" stroke-width="2" stroke-miterlimit="10" />
         <path d="M16 15.3543L1.64575 1" stroke="#131316" stroke-width="2" stroke-miterlimit="10" />
     </svg>
 </template>
+
+<style scoped>
+.base-icon {
+    width: 15px;
+    min-width: 15px;
+}
+</style>
