8b5373389d21ab105229308a147a99b8ef783ece
Alan Lima <dev.alanlima@gmail.com>
2023-10-12 12:33:37 -0300

    feat: Create component to social icons

---
+++ src/components/molecules/MolSocials.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="social-wrapper">
+    <li
+      v-for="(item, index) in props.items"
+      :key="index"
+    >
+      <a :href="item.link">
+        <component :is="item.icon" />
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+interface IconLink {
+  icon: object;
+  link: string
+}
+
+interface Props {
+  items: Array<IconLink>
+}
+
+const props = defineProps<Props>()
+
+</script>
+
+<style scoped>
+.social-wrapper {
+  display: flex;
+  gap: 16px;
+}
+</style>
